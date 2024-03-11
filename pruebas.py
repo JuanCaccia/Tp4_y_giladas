c5fae3c0d665cf71e1be17ea5f8e3c2bfacbab4d
JMCgit <juanmacaccia@gmail.com>
2024-03-11 15:43:21 -0300

    Manejo de ficheros

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,6 @@
 from main import menu as god_menu, shell_sort_vector as shell
 from datetime import datetime, timedelta
-import random
+import random, json
 n_1 = 5
 n_2 = 1
 # n_2 = "1"
@@ -8,12 +8,12 @@ n_2 = 1
 numeros = [49, 73, 11, 30, 82, 67, 5, 91, 62, 14, 20, 33, 55, 88, 42, 97, 2, 78, 60, 25]
 
 try:
-    print("pindongan´t")
     print(f"las suma es: {n_1 + n_2}")
+    print("pindongan´t")
 
 except TypeError as error:
-    print("pindonga")
     print(error)
+    print("pindonga")
 
 '''
 except Exception as error:
@@ -135,4 +135,91 @@ def menor_50(numero):
 
 
 print(list(filter(menor_50, numeros)))
+print(list(filter(lambda num: num < 50, numeros)))  # con lambdas
 
+# sets
+
+sets = {2, 4, 9, 0, 12}
+
+sets.add(2)
+sets.add(5)
+sets.add(17)
+sets.add(4)
+
+'''
+Ordenación y Duplicados:
+
+Listas: Mantienen el orden de los elementos y permiten duplicados. Puedes acceder a los elementos de una lista 
+utilizando índices.
+Sets: No mantienen ningún orden específico y no permiten duplicados. Los elementos en un conjunto están únicos.
+
+Sintaxis:
+
+Listas: Se definen utilizando corchetes [] y los elementos se separan por comas.
+Sets: Se definen utilizando llaves {} o la función set() y los elementos se separan por comas.
+
+Mutabilidad:
+
+Listas: Son mutables, lo que significa que puedes cambiar, agregar o eliminar elementos después de crear la lista.
+Sets: Son mutables, pero los elementos individuales no son mutables. No puedes cambiar un elemento específico en un 
+conjunto, pero puedes agregar o eliminar elementos.
+Operaciones:
+
+Listas: Ofrecen una variedad de operaciones como indexación, rebanado, concatenación y métodos específicos como append()
+, extend(), insert(), remove(), pop(), entre otros.
+Sets: Son útiles para realizar operaciones de conjuntos como unión, intersección, diferencia, etc. Los sets también
+tienen métodos para agregar elementos (add()), eliminar elementos (remove(), discard()), comprobar la membresía (in),etc
+
+Rendimiento:
+
+Listas: Las listas tienen un rendimiento eficiente para la indexación y el acceso a elementos por posición, pero pueden 
+ser menos eficientes para comprobar la existencia de un elemento debido a que se recorre secuencialmente.
+Sets: Los sets tienen un rendimiento muy eficiente para comprobar si un elemento está presente, ya que utilizan una
+estructura de datos hash subyacente para el almacenamiento.
+
+En resumen, las listas son una buena opción cuando necesitas una colección ordenada y con elementos duplicados, mientras
+que los sets son útiles cuando necesitas garantizar la unicidad de elementos y/o realizar operaciones de conjuntos 
+eficientes.
+'''
+
+if 2 in sets:
+    print("si esta wachin")
+
+print(sets)
+
+
+# manejo de ficheros
+
+texto = open("texto.txt", "r+")  # El 'r+' no sobreescribe un nuevo archivo, en cambio el 'w+' si lo hace
+
+# texto.writelines("\npinga pinga pinga pinga pinga")  # cadda vez que se ejecuta el programa agrega una linea xd
+# texto.write("\notra linea")
+
+texto.close()
+with open("texto.txt", "r+") as leer_texto:
+    for x in leer_texto:
+        print(x[:-1])
+
+
+json_texto = open("texto.json", "r+")
+
+json_test = {
+    "Nombre": "Juan",
+    "Apellido": "Caccia",
+    "Edad": 19,
+    "Apodo": "GODinez",
+    "Hobies": ["Leer", "Ir al gym", "Rodar la caprichosa"]
+}
+
+json.dump(json_test, json_texto, indent=4)
+
+json_texto.close()
+
+with open("texto.json", "r+") as lectura_json:
+    for line in lectura_json:
+        print(line[:-1])
+
+json_dict = json.load(open("texto.json", "r+"))
+print(json_dict)
+
+print(json_dict["Nombre"])
